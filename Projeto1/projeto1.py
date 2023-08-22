b5b0127930f84fa29f13ec3945d25cc42b58410e
amanjonson22 <amandacsr10@gmail.com>
2023-08-22 15:59:27 -0300

    Consertando input-projeto1

--- Projeto1/projeto1.py
+++ Projeto1/projeto1.py
@@ -5,7 +5,7 @@ def guess(x):
     guess = 0
     
     while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}'))
+        guess = int(input(f'Guess a number between 1 and {x}: '))
         if guess < random_number:
             print("Sorry, guess again. Too low.")
         elif guess > random_number:
