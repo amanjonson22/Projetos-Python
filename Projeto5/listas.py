8eb1d902bbae6727ff5bb24eacc1f628f0a28fb9
amanjonson22 <amandacsr10@gmail.com>
2023-08-24 20:34:24 -0300

    continuando programação do projeto5. adicionado informações do SOP

---
+++ Projeto5/listas.py
@@ -0,0 +1,27 @@
+limites = ['Máximo componente de vento de través: ', 'Decolagem/Pouso em voo duplo comando - 13 kt', 'Decolagem/Pouso em voo solo de aluno PCA - 08 kt', 'Decolagem/Pouso em voo solo de aluno PPA - 06 kt', 'Máx. componente de cauda para decolagem/pouso - 10 kt']
+
+oleo = ['Volume mínimo - 2,5 L', 'Pressão máxima - 102 psi', 'Pressão normal - 29 a 73 psi', 'Pressão mínima - 12 psi', 'Temperatura máxima - 130°C', 'Temperatura desejável - 90°C a 110°C', 'Temperatura mínima - 50°C']
+
+velocidades = ['Máx. estrutural (VNE) - 117 kt', 'Máx. operacional (VNO)- 107 kt', 'Máx. de manobra (VA) - 88 kt', 'Máx. para operação Full flap (VFE) - 78 kt', 'Velocidade de estol sem flap (Vs) - 45kt', 'Velocidade de estol com flap 1 (Vs1) - 40kt', 'Velocidade de estol com flap 2 (Vs0) - 39kt', 'Velocidade melhor razão de subida (Vy) - 65kt', 'Velocidade melhor ângulo de subida (Vx) - 55kt', 'Velocidade de melhor planeio (Vg) - 60kt']
+
+motor = ['Potência máxima (5.800 RPM) - 98,5 hp', 'Potência Max Continua (5.500 RPM) - 90 hp', 'Temperatura Máx. Coolant - 120°C', 'Temperatura máxima CHT - 135°C']
+
+sop = ['Os manuais do tipo SOP estabelecem como, quando e por quem as tarefas na cabine devem ser executadas na condução de um voo normal.', 'Função do SOP: garantir a segurança e eficiência das operações.','Os SOPs possibilitam a operação em um ambiente altamente complexo e sujeito a mudanças rápidas, com entendimento das atribuições designadas e de qual maneira cada tarefa será executada.', 'O SOP foi desenvolvido para que a operação seja conduzida da forma mais segura e eficiente, baseada em recomendações de fabricante, boas práticas da indústria, e na experiência operacional da empresa.']
+
+trip = ['No SOP, as seguintes referências serão utilizadas para definir membros de tripulação:', 'AL = Aluno/a','IN = Instrutor/a de voo']
+
+check = ['- Challenge/Response: Este checklist requer que o piloto verbalize e conforme visualmente cada item', '- Silent: Este checklist é executado em silêncio pelo piloto', '- Read and Do: Neste checklist, as ações são lidas e executadas na sequência. Cada item do checklist deverá ser lido em voz alta, a ação relacionada executada e em seguida a resposta verbalizada.', 'O piloto deverá anunciar']
+
+before_takeoff = ['O Before Takeoff Checklist deve ser executado novamente sempre que:','- Ocorra demora significativa entre a leitura do checklist e a autorização de decolagem', '- Houver mudança de pista após a leitura do checklist.']
+
+execucao = ['"Before Takeoff Checklist"','Ler, verificar visualmente e responder verbalmente à configuração, verificando se está de acordo com o definido pelo checklist lido','"Down to the Line"','Executar ações necessárias para finalização do checklist. Neste exemplo, aguardar a autorização de decolagem.','Quando recebida a autorização, seguir:','"Below the Line"','Ler, verificar visualmente e responder verbalmente à configuração verificando se está de acordo com o definido pelo checklist lido','"Before Takeoff Checklist Complete"']
+
+transferencia = ['A primeiro e mais importante recomendação de segurança:','É preponderante definir a todo tempo qual piloto tem os controles do voo.','Na significativa maioria das vezes o aluno exerce simultaneamente as duas funções: voar e monitorar','As funções são:','Pilot Flying(PF)','Pilot Monitoring(PM)','Situações como consulta a cartas, preenchimento de planilhas de navegação ou realização de briefing podem exigir que o aluno transfira temporariamente os controles e a comunicação ao Instrutor.','Em situações que envolvam possível risco à segurança de voo, o Instrutor também poderá proativamente determinar a transferência de controles.', 'Qualquer mudança de função deve ser verbalmente anunciada e contfirmada.','Antes da transferência de controles, deve-se atualizar o outro piloto sobre o status da aeronave:','- Altitude','- Velocidade','- Proa','- Órgão ATC', '- Possíveis restrições']
+
+nav = ['NAV:','Dia: Desligada','Noite: Sempre ligada com a aeronave energizada']
+
+beacon = ['BEACON:','Da autorização para o acionamento até a parada completa do motor após o corte.']
+
+landing = ['LANDING:','Dia: da autorização de decolagem até 400 ft AGL. Para pouso, do landing flow até livrar a pista.','Noite: Do início do taxi para a decolagem até o After Takeoff Checklist e da aproximação para pouso até o corte']
+
+strobe = ['STROBE:', 'Dia: Desligada','Noite: Do ingresso na pista para decolagem até livrar a pista após o pouso. Também deve ser ligada para cruzamento de pistas.']
--- Projeto5/projeto5.py
+++ Projeto5/projeto5.py
@@ -1,3 +1,19 @@
+from listas import limites
+from listas import oleo
+from listas import velocidades
+from listas import motor
+from listas import sop
+from listas import trip
+from listas import check
+from listas import before_takeoff
+from listas import execucao
+from listas import transferencia
+from listas import nav
+from listas import beacon
+from listas import landing
+from listas import strobe
+
+
 def limites():
     info_lista = 0
     voltar = False
@@ -19,7 +35,6 @@ def limites():
             print('Peso Máximo de Taxi & Rampa - 600 kg\n') 
         elif querer == 'lo':
             print('\nLimites operacionais\n')
-            limites = ['Máximo componente de vento de través: ', 'Decolagem/Pouso em voo duplo comando - 13 kt', 'Decolagem/Pouso em voo solo de aluno PCA - 08 kt', 'Decolagem/Pouso em voo solo de aluno PPA - 06 kt', 'Máx. componente de cauda para decolagem/pouso - 10 kt']
             for i in limites:
                 print(limites[info_lista])
                 info_lista += 1
@@ -28,38 +43,323 @@ def limites():
             print('Combustível utilizável por tanque - 68L\n')
         elif querer == 'óleo':
             print('\nÓleo\n')
-            óleo = ['Volume mínimo - 2,5 L', 'Pressão máxima - 102 psi', 'Pressão normal - 29 a 73 psi', 'Pressão mínima - 12 psi', 'Temperatura máxima - 130°C', 'Temperatura desejável - 90°C a 110°C', 'Temperatura mínima - 50°C']
-            for i in óleo:
-                print(óleo[info_lista])
+            for i in oleo:
+                print(oleo[info_lista])
                 info_lista += 1
         elif querer == 'vc':
             print('\nVelocidades Características\n')
-            velocidades = ['Máx. estrutural (VNE) - 117 kt', 'Máx. operacional (VNO)- 107 kt', 'Máx. de manobra (VA) - 88 kt', 'Máx. para operação Full flap (VFE) - 78 kt', 'Velocidade de estol sem flap (Vs) - 45kt', 'Velocidade de estol com flap 1 (Vs1) - 40kt', 'Velocidade de estol com flap 2 (Vs0) - 39kt', 'Velocidade melhor razão de subida (Vy) - 65kt', 'Velocidade melhor ângulo de subida (Vx) - 55kt', 'Velocidade de melhor planeio (Vg) - 60kt']
             for i in velocidades:
                 print(velocidades[info_lista])
                 info_lista += 1
         elif querer == 'motor':
             print('\nMotor\n')
-            motor = ['Potência máxima (5.800 RPM) - 98,5 hp', 'Potência Max Continua (5.500 RPM) - 90 hp', 'Temperatura Máx. Coolant - 120°C', 'Temperatura máxima CHT - 135°C']
             for i in motor:
                 print(motor[info_lista])
                 info_lista += 1
         info_lista = 0
 
+def exterior():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('\n#Voltar')
+
+def bs():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('\n#Voltar')
+
+def es():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('\n#Voltar')
+
+def a_s():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('\n#Voltar')
+
+def taxi():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('#Adicional')
+    print('\n#Voltar')
+
+def bt():
+    print('\n#Checklist - Antes de receber autorização(antes)')
+    print('#Flow expandido - Antes(expandido antes)')
+    print('#Checklist - Depois de receber autorização(depois)')
+    print('#Flow expandido - depois(expandido depois)')
+    print('#Adicionais')
+    print('\n#Voltar')
+
+def nt():
+    print('\n#Generalidades')
+    print('#Normal Takeoff Actions and Callouts(actions)')
+    print('#After Takeoff Checklist')
+    print('\n#Voltar')
+
+def cruise():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('#Adicionais')
+    print('\n#Voltar')
+
+def descent():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('#Adicionais')
+    print('\n#Voltar')
+
+def pouso():
+    print('\n#Tail Strike(tail)')
+    print('#Hard Landing')
+    print('#Bounced Landing(bounced)')
+    print('#Pouso com vento de través')
+    print('#Uso dos freios(freio)')
+    print('\n#Voltar')
+
+def landing_checklist():
+    print('\n#Landing Checklist(landing)')
+    print('#Aproximação estabilizada(aproximação)')
+    print('#Pouso')
+    print('\n#Voltar')
+
+def landing():
+    print('\n#Generalidades')
+    print('#Checklist')
+    print('#Flow expandido(expandido)')
+    print('#Adicionais')
+    print('\n#Voltar')
+    querer = input('Qual informação você quer? ').lower()
+    if querer == 'checklist':
+        landing_checklist()
+
+def al():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('\n#Voltar')
+
+def parking():
+    print('\n#Checklist')
+    print('#Flow expandido(expandido)')
+    print('\n#Voltar')
+
 def checklists():
-    um = 1
+    print('\n#Exterior Walkaround(exterior)')
+    print('#Before start(bs)')
+    print('#Engine Start(es)')
+    print('#After Start(as)')
+    print('#Táxi')
+    print('#Before Takeoff(bt)')
+    print('#Normal Takeoff(nt)')
+    print('#Cruise')
+    print('#Descent')
+    print('#Go Around(ga)')
+    print('#After Landing(al)')
+    print('#Paring')
+    print('#Securing')
+    print('\n#Voltar')
+    querer = input('Qual informação você quer? ').lower()
+    if querer == 'exterior':
+        exterior()
+    elif querer == 'bs':
+        bs()
+    elif querer == 'es':
+        es()
+    elif querer == 'as':
+        a_s()
+    elif querer == 'taxi':
+        taxi()
+    elif querer == 'bt':
+        bt()
+    elif querer == 'nt':
+        nt()
+    elif querer == 'cruise':
+        cruise()
+    elif querer == 'descent':
+        descent()
+    elif querer == 'ga':
+        um = 1
+    elif querer == 'al':
+        al()
+    elif querer == 'parking':
+        parking()
+    elif querer == 'securing':
+        um = 1
 
 def briefings():
-    um = 1 
+    print('\n#Briefing de Decolagem - TOSE (decolagem)')
+    print('#Briefing de aproximação - ANFLACRAFTS(Aproximação)')
+    print('#Briefing do aeroporto(aeroporto)')
+    print('#Briefing Check the Glass ou The Glass (glass)')
+    print('\n#Voltar')
+    querer = input('Qual informação você quer? ').lower()
 
+def filosofia_operacional():
+    voltar = False
+    info_lista = 0
+    while not voltar:
+        print('\n#Comunicação(com)')
+        print('#Tranferência de Controle e Comunicações(transferencia)')
+        print('#Utilização de Luzes Externas(luz)')
+        print('\n#Voltar\n')
+        querer = input('Qual informação você quer? ').lower()
+        if querer == 'com':
+            print('\nComunicação\n')
+            print('A comunicação eficiente entre Aluno e Instrutor é vital para uma boa coordenação de cabine.')
+            print('Sempre que um piloto receber informações atualizadas ou executar qualquer ajuste em um equipamento, deverá informar ao(à) outro(a) piloto e receber confirmação de ciência sobre a mudança em questão.')
+        elif querer == 'transferencia':
+            print('\nTransferência de Controle e Comunicações\n')
+            for i in transferencia:
+                print(transferencia[info_lista])
+                info_lista += 1
+            info_lista = 0
+            print('AL: "Your Controls and ATC"','\nIN: "My Controls and ATC"')
+            print('\nAL: "Your Controls, My ATC"','\nIN:"My controls, your ATC"')
+            print('\nAL:"My controls and ATC"','\nIN: "Your controls and ATC"')
+            print('\nAL: "My controls, your ATC"','\nIN: "Your controls, my ATC"')
+        elif querer == 'luz':
+            print('\nUtilização de Luzes Externas\n')
+            for i in nav:
+                print(nav[info_lista])
+                info_lista += 1
+            info_lista = 0
+            for i in beacon:
+                print(beacon[info_lista])
+                info_lista += 1
+            info_lista = 0
+            for i in landing:
+                print(landing[info_lista])
+                info_lista += 1
+            info_lista = 0
+            for i in strobe:
+                print(strobe[info_lista])
+                info_lista += 1
+            info_lista = 0
+            print('\nNOTA\n')
+            print('Durante backtrack, cruzamento e/ou taxi sobre a pista em uso, utilizar a configuração de luzes aplicável para a decolagem, e manter até que a pista seja completamente liberada.')
+        elif querer == 'voltar':
+            print('\nVoltar\n')
+            voltar = True
+            return voltar
+        info_lista = 0
+
+def adicionais():
+    voltar = False
+    info_lista = 0
+    while not voltar:
+        print('\n#Filosofia do SOP(SOP)')
+        print('#Tripulação')
+        print('#Filosofia de leitura do Checklist(checklist)')
+        print('#Filosofia Operacional(operacional)')
+        #NÃO FUNCIONANDO AINDA ----> print('#Flight Mode Annunciator(FMA)')
+        #NÃO FUNCIONANDO AINDA ----> print('#Callouts')
+        print('\n#Voltar\n')
+        querer = input('Qual informação você quer? ').lower()
+        if querer == 'sop':
+            print('\nFilosofia do SOP (Standard Operanting Procedures)')
+            for i in sop:
+                print(sop[info_lista])
+                info_lista += 1
+        elif querer == 'tripulação':
+            print('\nTripulação\n')
+            for i in trip:
+                print(trip[info_lista])
+                info_lista += 1
+        elif querer == 'checklist':
+            print('\nFilosofia de leitura do Checklist\n')
+            print('O uso correto dos Normal Checklists reduz a incidência de práticas inseguras, não padronizadas e/ou desenvolvimento de procedimentos informais.')
+            for i in check:
+                print(check[info_lista])
+                info_lista += 1
+            info_lista = 0
+            print('O piloto deverá anunciar o nome do checklist antes de iniciar a sua leitura. Ao término, deverá anunciar o nome do checklist, seguido por "Complete"')
+            print('Down the line/Below the line - Quando um checklist for segregado em dois momentos, o piloto deve anunciar "Down the Line" ao atingir a linha separadora. Para realizar a segunda parte do checklist, deve anunciar o "Below the Line"')
+            for i in before_takeoff:
+                print(before_takeoff[info_lista])
+                info_lista += 1
+            info_lista = 0
+            print('\nA execução dos Normal Checklist deve ocorrer conforme o seguinte exemplo:')
+            for i in execucao:
+                print(execucao[info_lista])
+                info_lista += 1
+        elif querer == 'operacional':
+            print('\nFilosofia Operacional\n')
+            filosofia_operacional()
+        elif querer == 'fma':
+            print('\nFlight Mode Annunciator\n')
+        elif querer == 'callouts':
+            print('\nCallouts\n')
+        elif querer == 'voltar':
+            print('\nVoltar')
+            voltar = True
+            return voltar
+        info_lista = 0
+
+def curta():
+    print('\n#Actions e Callouts(actions)')
+    print('#Tabela de performance de decolagem curta(performance)')
+
+def cold():
+    print('\n#Formação de Gelo no Carburador(gelo)')
+    print('#Utiliza da Tabela de Probabilidade de Formação de gelo(tabela)')
+    print('#Em caso de suspeita de formação de gelo(suspeita)')
+
+def procedimentos():
+    print('\n#Táxi')
+    print('#Acionamento Manual - Giro de Hélice(manual)')
+    print('#Decolagem Curta ou pista em grama(curta)')
+    print('#Performance de subida(subida)')
+    print('#Consumo em Cruzeiro de Velocidade(consumo)')
+    print('#Cold Weather Operation(cold)')
+    print('#Dive and Drive')
+    querer = input('Qual informação você quer? ').lower()
+    if querer == 'curta':
+        curta()
+    elif querer == 'cold':
+        cold()
+
+def motor_aciona():
+    print('\n#Motor acionado(acionado)')
+    print('#Knocking under load(knocking)')
+    print('#Motor áspero após período de aquecimento, emissão de fumaça pelo escapamento(aspero)')
+    querer = input('Qual informação você quer? ').lower()
+
+def emergência_oleo():
+    print('\n#Baixa pressão')
+    print('#Alta pressão')
+
+def rotax():
+    print('\n#Motor não aciona(não)')
+    print('#Motor aciona(aciona)')
+    print('#Pressão de Óleo (óleo)')
+    print('#Nível de óleo(nivel)')
+    print('#Acionamento frio do motor(frio)')
+    querer = input('Qual informação você quer? ').lower()
+    if querer == 'aciona':
+        motor_aciona()
+    elif querer == 'óleo':
+        emergência_oleo()
+    elif querer == 'nivel':
+        um = 1
+
+def emergência():
+    print('\n#Geral')
+    print('#Troubleshooting conforme ROTAX(ROTAX)')
+    querer = input('Qual informação você quer? ').lower()
+    if querer == 'rotax':
+        rotax()
 
 def montaer():
     sair = False
     while not sair:
         print('\nInformações disponíveis:')
         print('#Limites')
-        print('#Checklists')
-        print('#Briefings')
+        #NÃO FUNCIONANDO AINDA ----> print('#Checklists')
+        #NÃO FUNCIONANDO AINDA ----> print('#Briefings')
+        print('#Formato de Leitura e informações adicionais(adicionais)')
+        #print('#Procedimentos Adicionais e Performance(procedimentos)')
+        #NÃO FUNCIONANDO AINDA ----> print('#Procedimentos anormais e de emergência(emergência)')
         print('#Sair\n')
         informação = input('Qual informação você quer? ').lower()
         if informação == 'limites':
@@ -68,6 +368,12 @@ def montaer():
             checklists()
         elif informação == 'briefings':
             briefings()
+        elif informação == 'adicionais':
+            adicionais()
+        elif informação == 'procedimentos':
+            procedimentos()
+        elif informação == 'emergência':
+            emergência()
         elif informação == 'sair':
             sair = True
             return sair
